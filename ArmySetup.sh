9ad8647dd33a5e5dfe1f700caf0217cfa255e227
Chris Hewitt <37228411+C0ntr07@users.noreply.github.com>
2022-04-14 10:13:34 -0400

    Improve system flexibility
    addressed systems without systemctl and change group without reboot

--- ArmySetup.sh
+++ ArmySetup.sh
@@ -3,13 +3,18 @@ sudo apt update
 sudo apt install -y docker.io
 sudo systemctl enable docker --now
 docker
-sudo usermod -aG docker $USER
+# sudo usermod -aG docker $USER
+newgrp docker
 printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list ; curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl unmask docker.service
 sudo systemctl unmask docker.socket
 sudo systemctl start docker.service
+if [ ! $? -eq 0 ]; then
+  # if systemctl not loaded SERVICE has to be used.  This is true for WSL
+  sudo service docker.service start
+fi
 sudo chmod 666 /var/run/docker.sock
 sudo docker run hello-world
 docker run -it --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy -t 2000 --itarmy --debug
