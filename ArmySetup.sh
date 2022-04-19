c951397ce2970874f3e02fd9d355e5e38247b194
Chris Hewitt <37228411+C0ntr07@users.noreply.github.com>
2022-04-18 20:10:31 -0400

    Update ArmySetup.sh

--- ArmySetup.sh
+++ ArmySetup.sh
@@ -4,7 +4,7 @@ if ! which docker > /dev/null; then
   sudo apt install -y docker.io
 fi
 sudo systemctl enable docker --now
-docker
+# docker
 # sudo usermod -aG docker $USER
 newgrp docker
 printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" | sudo tee /etc/apt/sources.list.d/docker-ce.list ; curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-ce-archive-keyring.gpg
